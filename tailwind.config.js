4922c5f03eb09241f06bc877543e8b3ef5bf2714
SABIN PANTHI <103927757+071Sabin@users.noreply.github.com>
2026-02-27 15:11:58 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./app/**/*.php",
+    "./vendor/livewire/**/*.blade.php",
+    "./vendor/rappasoft/**/*.blade.php",
+  ],
+  safelist: [
+    "translate-x-0",
+    "-translate-x-full",
+    "translate-x-full",
+    "overflow-hidden",
+    "fixed",
+    "inset-0",
+  ],
+}
